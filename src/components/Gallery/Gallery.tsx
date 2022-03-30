ba9fe15c9b0cf990cdacf96c2abdff1b86c44461
Kishan-Pitroda <kishanpitroda111.kp@gmail.com>
2022-03-30 16:38:08 +0530

    final personal photo upload

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,6 +1,174 @@
 import React from "react";
+import Gallery from "react-photo-gallery";
 
-const Gallery: React.FC = () => {
-  return <div>Gallery</div>;
+export const photos = [
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(1).jpeg?raw=true",
+    height: 3,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(2).jpeg?raw=true",
+    height: 3,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(3).jpeg?raw=true",
+    height: 3,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(4).jpeg?raw=true",
+    height: 3,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(4).JPG?raw=true",
+    height: 5,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(5).JPG?raw=true",
+    height: 5,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(1).JPG?raw=true",
+    height: 4,
+    width: 3,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(3).JPG?raw=true",
+    height: 5,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(2).JPG?raw=true",
+    height: 5,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(6).jpeg?raw=true",
+    height: 5,
+    width: 3,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(6).jpg?raw=true",
+    height: 1,
+    width: 1,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(7).jpg?raw=true",
+    height: 5,
+    width: 3,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(8).jpg?raw=true",
+    height: 3,
+    width: 5,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(9).jpg?raw=true",
+    height: 5,
+    width: 3,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(10).jpg?raw=true",
+    height: 5,
+    width: 3,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(11).jpg?raw=true",
+    height: 4,
+    width: 5,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(12).jpg?raw=true",
+    height: 5,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(13).jpg?raw=true",
+    height: 5,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(14).jpg?raw=true",
+    height: 5,
+    width: 3,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(15).jpg?raw=true",
+    height: 4,
+    width: 5,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(16).jpg?raw=true",
+    height: 5,
+    width: 3,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(17).jpg?raw=true",
+    height: 5,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(18).jpg?raw=true",
+    height: 5,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(19).jpg?raw=true",
+    height: 4,
+    width: 5,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(20).jpg?raw=true",
+    height: 5,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(21).jpg?raw=true",
+    height: 4,
+    width: 5,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(22).jpg?raw=true",
+    height: 4,
+    width: 5,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(23).jpg?raw=true",
+    height: 5,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(24).jpg?raw=true",
+    height: 5,
+    width: 4,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(25).jpg?raw=true",
+    height: 5,
+    width: 3,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(26).jpg?raw=true",
+    height: 5,
+    width: 3,
+  },
+  {
+    src: "https://github.com/Kishan-Pitroda/My-Portfolio/blob/pwa-integration/src/assets/gallery/personal/img%20(27).jpg?raw=true",
+    height: 5,
+    width: 3,
+  },
+];
+
+const GalleryComponent: React.FC = () => {
+  return (
+    <div>
+      <Gallery photos={photos} />
+    </div>
+  );
 };
-export default Gallery;
+export default GalleryComponent;
